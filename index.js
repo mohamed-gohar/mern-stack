d62d1df890f4d1b1a9e47ee5f64b709322c7461f
Mohamed Gohar <mymohamedgohar@gmail.com>
2021-08-07 20:19:53 +0200

    add static express

--- index.js
+++ index.js
@@ -29,12 +29,10 @@ app.use((req, res) => {
 });
 
 //static builds
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // start server
 const port = process.env.PORT || 5000;
