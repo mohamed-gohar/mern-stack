2e17ab5457e5f05bc25e950516c0c52fe2763451
Mohamed Gohar <mymohamedgohar@gmail.com>
2021-08-07 22:06:28 +0200

    add static express

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 //static files
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
