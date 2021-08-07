ec54d2e9785796952c552a9fb168065957cd26a5
Mohamed Gohar <mymohamedgohar@gmail.com>
2021-08-07 21:29:16 +0200

    add static express

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: false }));
 
 //static files
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 //connect mongodb
 const mongoURI = process.env.MONGO_URI;
